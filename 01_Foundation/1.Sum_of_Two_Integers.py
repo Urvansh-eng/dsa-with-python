e5bed2e5ffba397c60b2fa21c9be71daa69b98f2
Urvansh-eng <kken11350@gmail.com>
2026-06-14 15:50:53 +0530

    Solve Foundation Q1 - Sum of Two Integers

---
+++ 01_Foundation/1.Sum_of_Two_Integers.py
@@ -0,0 +1,19 @@
+# Problem Statement:
+# Write a Program that defines a function to calculate the sum of two integers and prints the result. Call this function by passing two integer values.
+
+
+# Approach:
+# create a function that accept two integer and returns their sum
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter Second number: "))
+
+
+def add(num1,num2):
+    return  num1+num2
+    
+
+print(add(num1,num2))
+
+
